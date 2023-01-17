1cb395e2d8c3fa886633c8cdcdea6b56d0215efe
pengshiyu <1940607002@qq.com>
2023-01-17 17:51:40 +0800

    fix

--- mysync/util/config_util.py
+++ mysync/util/config_util.py
@@ -69,7 +69,10 @@ def parse_config(args):
     for temp_config in config_list:
         default_config['input'].update(temp_config['input'])
         default_config['output'].update(temp_config['output'])
-        default_config['pipeline'].update(temp_config['pipeline'])
+
+        # 可选
+        if temp_config.get('pipeline'):
+            default_config['pipeline'].update(temp_config['pipeline'])
 
     pprint(default_config)
 
